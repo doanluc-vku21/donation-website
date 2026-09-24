1241ec2348af3dd9a577a05650cdd62dd8bb4ebf
doanluc-vku21 <doanluc197@gmail.com>
2026-09-24 16:14:59 +0700

    remove orga

--- components/campaign/site-footer.tsx
+++ components/campaign/site-footer.tsx
@@ -153,10 +153,7 @@ export function SiteFooter({
                 "Open Hands Relief"}
             </p>
 
-            <p className="mt-1">
-              {content.footerSubtitle ||
-                "Sample organization · UI preview"}
-            </p>
+            
           </div>
 
           {/* LEGAL LINKS */}
